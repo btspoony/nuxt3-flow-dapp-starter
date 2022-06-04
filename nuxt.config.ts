bc546b278f40560beac5c702a774734d95009454
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-06-04 12:05:04 +0800

    feat: add fcl d.ts, add polyfills

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 import { defineNuxtConfig } from "nuxt";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -20,29 +24,56 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  build: {
-    transpile: ["@heroicons/vue"],
-  },
   // installed modules
   modules: [
     // Doc: https://github.com/nuxt-community/tailwindcss-module
     "@nuxtjs/tailwindcss",
   ],
+  build: {
+    transpile: ["@heroicons/vue"],
+  },
   // vite configure
   vite: {
     // raw assets
     assetsInclude: ["**/*.cdc"],
+    // plugins
+    plugins: [
+      // ↓ Needed for development mode
+      !isProduction &&
+      nodePolyfills({
+        include: [
+          "node_modules/**/*.js",
+          new RegExp("node_modules/.vite/.*js"),
+        ],
+      }),
+    ],
     // Dependency Pre-Bundling
     optimizeDeps: {
       esbuildOptions: {
+        // Node.js global to browser globalThis
         define: {
           global: "globalThis",
         },
+        // Enable esbuild polyfill plugins
         plugins: [
           NodeGlobalsPolyfillPlugin({
+            process: true,
             buffer: true,
           }),
+          NodeModulesPolyfillPlugin(),
         ],
+      },
+    },
+    build: {
+      rollupOptions: {
+        plugins: [
+          // ↓ Needed for build
+          nodePolyfills(),
+        ],
+      },
+      // ↓ Needed for build if using WalletConnect and other providers
+      commonjsOptions: {
+        transformMixedEsModules: true,
       },
     },
   },
