7c80c1d57659d032be4f3c503e49fc7ab12bc5e2
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-06-23 15:04:54 +0800

    chore: update tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,39 @@
+const defaultSans = [
+  // English Fonts
+  "Overpass",
+  "Roboto",
+  "'Helvetica Neue'",
+  "Arial",
+  "Helvetica",
+  "'Noto Sans'",
+  // Chinese Fonts, Mac/Win/Linux
+  "'PingFang SC'",
+  "'Microsoft Yahei'",
+  "微软雅黑",
+  "'WenQuanYi Micro Hei'",
+  // System Fonts
+  "system-ui",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "'Segoe UI'",
+  "sans-serif",
+  // emoji fonts
+  "'Apple Color Emoji'",
+  "'Segoe UI Emoji'",
+  "'Segoe UI Symbol'",
+  "'Noto Color Emoji'",
+];
+
 module.exports = {
   theme: {
-    fontFamily: {
-      sans: [
-        // English Fonts
-        "Overpass",
-        "Roboto",
-        "'Helvetica Neue'",
-        "Arial",
-        "Helvetica",
-        "'Noto Sans'",
-        // Chinese Fonts, Mac/Win/Linux
-        "'PingFang SC'",
-        "'Microsoft Yahei'",
-        "微软雅黑",
-        "'WenQuanYi Micro Hei'",
-        // System Fonts
-        "system-ui",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "'Segoe UI'",
-        "sans-serif",
-        // emoji fonts
-        "'Apple Color Emoji'",
-        "'Segoe UI Emoji'",
-        "'Segoe UI Symbol'",
-        "'Noto Color Emoji'",
-      ],
-    },
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [...defaultSans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
